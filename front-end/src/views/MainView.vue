9a29701cf9153ce1115548fd0d81bb9af2fa73ba
Pierre-Emmanuel <pierre-emmanuel.marrel@etu.univ-lyon1.fr>
2024-12-06 07:21:04 +0100

    Quizs

--- front-end/src/views/MainView.vue
+++ front-end/src/views/MainView.vue
@@ -4,10 +4,9 @@ import Woman from '../components/Woman.vue';
 import HelloWorld from '../components/HelloWorld.vue';
 import FishingGame from '../components/FishingGame.vue';
 import game_plastique from '@/components/game_plastique.vue';
-import DynamicPage from '../components/DynamicPage.vue';
+import QuizPage from '../components/QuizPage.vue';
 import StoryPopup from '../components/StoryPopup.vue';
 import factoriesGames from '../components/factoriesGames.vue';
-
 
 let story_message = ref("L'océan, vaste et mystérieux, existe depuis des millénaires. Il a traversé les âges, portant en son sein la naissance et l'évolution de formes de vie infiniment diverses. Comme les cellules d'un corps vivant, ces créatures marines se sont multipliées, chacune apportant une spécificité unique à l'ensemble de cet écosystème fascinant.");
 let show_story_popup = ref(true);
@@ -16,7 +15,6 @@ function show_story(message){
   story_message.value = message;
   show_story_popup.value = true;
 }
-
 
 const womanVisible = ref(true);
 const currentComponent = ref(null);
@@ -46,6 +44,9 @@ const handleCloudSelection = (id) => {
     currentComponent.value = null;
     show_story("Bravo ! Vous avez protégé les glaciers !");
   }
+  else if (id == 1) {
+    currentComponent.value = QuizPage;
+  }
   
   else {
     currentComponent.value = null;
@@ -57,98 +58,35 @@ window.addEventListener('cloudSelected', (event) => {
   handleCloudSelection(event.detail);
 });
 
-const toggleValue = ref(false)
-
-const dynamicProps = ref({
-  someProp: 'Initial Prop Value',
-  anotherProp: 123,
-  q_nb : 1,
-  question: "Quel est l'un des principaux effets de la fonte des glaces polaires sur les océans ?",
-  questionProps: {
-    1 : "Augmentation de la salinité des océans",
-    2 : "Réduction du niveau de la mer",
-    3 : "Augmentation du niveau de la mer",
-    4 : "Diminution de la température de l'eau"
-  },
-  correctAnswer: 3,
-});
-
-function toggleComponent() {
-  toggleValue.value = !toggleValue.value;
-  if (toggleValue.value) {
-    dynamicProps.value = {
-      someProp: 'NOT Initial Prop Value',
-      anotherProp: 69400,
-    };
-  } else {
-    dynamicProps.value = {
-      someProp: 'LSKDHFLSKDHFL',
-      anotherProp: 80085,
-    };
-  }
-}
 </script>
 
 <template>
   <div>
     <Woman :class="{ hidden: !womanVisible }" />
 
-        <div v-if="currentComponent" class="dynamic-component">
-        <component :is="currentComponent" />
-        </div>
-        <button @click="toggleComponent()">
-          Switch component
-        </button>
-        <DynamicPage
-          upper-msg="Le cœur, lorsqu'il est affecté par l'hypertension, subit une pression intense. Cette même pression pèse sur les courants océaniques, perturbés par le réchauffement climatique, cela met en péril l'équilibre de notre planète. Voici un podcast pour en savoir plus." middle-title="OHOHOHO" 
-          dynamic-component="QuizPage" :dynamic-props="dynamicProps" />
-    
+    <div v-if="currentComponent" class="dynamic-component">
+      <component :is="currentComponent" />
+    </div>
   </div>
 
-  <StoryPopup :message="story_message" :show_footer="show_story_popup" @close_popup="show_story_popup = false"/>
+  <!-- Display the StoryPopup when show_story_popup is true -->
+  <StoryPopup :message="story_message" :show-footer="show_story_popup" @close_popup="show_story_popup = false" />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 .hidden {
   display: none;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .dynamic-component {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1000; /* Mettre au-dessus de Woman */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.dynamic-component {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1000; /* Ensure it appears above other components */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
