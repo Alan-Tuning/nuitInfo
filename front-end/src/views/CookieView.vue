e7ad4badf12a17001e351fcec88eb3c74d7bcfb8
Guillaume Jean-Luc <jean-luc.guillaume@etu.univ-lyon1.fr>
2024-12-06 02:43:12 +0100

    areyousure

--- front-end/src/views/CookieView.vue
+++ front-end/src/views/CookieView.vue
@@ -1,15 +1,17 @@
+
+
 <template>
     <div>
         <h1>CookieView</h1>
+        <Areyousure/>
     </div>
 </template>
 
 <script setup>
+    import Areyousure from '../components/Areyousure.vue';
     import { ref } from 'vue';
 </script>
 
 <style scoped>
-    h1 {
-        color: red;
-    }
+
 </style>
